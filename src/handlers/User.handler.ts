f8cc66f81eb6dd0d5df41dde84d9b03d56c94729
Matt Gallucci <mattgallucci97@gmail.com>
2022-12-20 12:37:56 -0800

    changed reference to user.username field to user.artistName, added grpc handler function

--- src/handlers/User.handler.ts
+++ src/handlers/User.handler.ts
@@ -1,39 +1,26 @@
 import { NextFunction, Request, Response } from 'express';
 import { createUser, findUserByEmail } from '../services/User.service';
 import { CreateUserInput } from '../database/schemas/User.schema';
+import { sendUnaryData, ServerUnaryCall } from '@grpc/grpc-js';
+import { GetUserForLoginRequest, GetUserForLoginResponse } from '../proto/user_pb';
 
-export const indexHandler = async (req: Request<{}, {}, {}, { email: string }>, res: Response, next: NextFunction) => {
-  if (!req.query) {
+export const indexHandler = async (req: Request, res: Response, next: NextFunction) => {
     return res.status(200).json({ message: 'User service online!' });
-  } else {
-    const email = req.query.email;
-    try {
-      const user = await findUserByEmail(email);
-      console.log(user);
-      if (!user) {
-        return res.status(401).json({ message: 'No user was found' });
-      }
-      // create the response variable with only needed fields
-      const userResponse = { id: user._id, email: user.email, username: user.username, password: user.password };
-      return res.status(200).json(userResponse);
-    } catch (err: any) {
-      console.error(err.code);
-      return res.status(401).json({ message: 'Invalid credentials' });
-    }
   }
 };
 
 export const registerUserHandler = async (req: Request<{}, {}, CreateUserInput>, res: Response, next: NextFunction) => {
-  const { email, username, password } = req.body;
-
+  const { email, password, artistName } = req.body;
+  console.log('artistName: ', artistName);
   try {
     const user = await createUser({
-      email: email.toLowerCase(),
-      username,
+      email,
+      artistName,
       password,
     });
 
-    console.log(`  --- user registered : ${username}  ---  `);
+    console.log(`  --- user registered : ${artistName}  ---  `);
+    // this needs to call the auth service and generate tokens for the new user here
     return res.status(200).json({ message: 'user registered succesfully', user });
   } catch (err: any) {
     if (err.code === '23505') {
@@ -41,5 +28,33 @@ export const registerUserHandler = async (req: Request<{}, {}, CreateUserInput>,
     }
     console.error(err);
     return res.status(500).json({ message: 'failed to create the user', err });
+  }
+};
+
+// TODO: create a directory dedicated to all gRPC mapped handler functions
+export const getUserForLogin = async (
+  call: ServerUnaryCall<GetUserForLoginRequest, GetUserForLoginResponse>,
+  callback: sendUnaryData<GetUserForLoginResponse>
+) => {
+  const email = call.request.getEmail();
+  try {
+    const user = await findUserByEmail(email);
+    console.log(user);
+    if (!user) {
+      return callback({
+        code: 5,
+        message: 'No user found with that email',
+      });
+    }
+    // create the response variable with only needed fields
+    const userResponse = new GetUserForLoginResponse();
+    userResponse.setId(user._id);
+    userResponse.setEmail(user.email);
+    userResponse.setArtistName(user.artistName);
+    userResponse.setPassword(user.password);
+    return callback(null, userResponse);
+  } catch (err: any) {
+    console.error(err.code);
+    callback(err.code);
   }
 };
