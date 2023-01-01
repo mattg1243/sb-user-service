287062ecf900fe2f317e7e1e41bdd5ae0959b30d
Matt Gallucci <mattgallucci97@gmail.com>
2023-01-01 12:00:01 -0800

    added jwt logic

---
+++ src/jwt.ts
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+
+interface IDecodedToken {
+  user: {
+    id: string;
+    email: string;
+    artistName: string;
+  };
+  iat: number;
+  exp: number;
+}
+
+// verify tokens
+export const verifyJwt = (token: string): IDecodedToken | null => {
+  // this needs optimization
+  let key = process.env.ACCESS_SECRET;
+  console.log('jwt key: \n', key);
+  if (!key) key = '';
+  try {
+    const key64 = Buffer.from(key, 'base64').toString('ascii');
+    const decoded = jwt.verify(token, key64) as IDecodedToken;
+
+    return decoded;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
