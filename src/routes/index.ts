f08a8fe210323e4c30bf010610f79cc224358f69
Matt Gallucci <mattgallucci97@gmail.com>
2023-01-01 12:01:41 -0800

    added routes for standard CRUD operations

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,25 @@
 import Router from 'express';
-import { indexHandler, registerUserHandler } from '../handlers/User.handler';
+import {
+  indexHandler,
+  registerUserHandler,
+  getUserHandler,
+  updateUserHandler,
+  getUserForLoginHTTP,
+} from '../handlers/User.handler';
 
 const router = Router();
 
-router.get('/', indexHandler);
+router.get('/', getUserHandler);
+router.post('/login', getUserForLoginHTTP);
 router.post('/register', registerUserHandler);
+router.post('/update', updateUserHandler);
+router.get(
+  '*',
+  (res, response, next) => {
+    console.log('random route hit, all others missed');
+    next();
+  },
+  getUserHandler
+);
 
 export default router;
