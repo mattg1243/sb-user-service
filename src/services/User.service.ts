c88c478c3c4a115e737eb027f3aa478568de2a13
Matt Gallucci <mattgallucci97@gmail.com>
2023-01-01 12:02:31 -0800

    added some query service functions

--- src/services/User.service.ts
+++ src/services/User.service.ts
@@ -8,9 +8,12 @@ const userRepository = AppDataSource.getRepository(User);
 export const createUser = async (input: CreateUserInput) => {
   return (await AppDataSource.manager.save(AppDataSource.manager.create(User, input))) as User;
 };
-
+// NOTE: this does return the password has while selecting by ID does NOT
 export const findUserByEmail = async (email: string, select?: {}) => {
-  return await userRepository.findOneBy({ email: email });
+  return await userRepository.findOne({
+    where: { email: email },
+    select: { _id: true, email: true, artistName: true, password: true },
+  });
 };
 
 export const findUserById = async (userId: string) => {
@@ -19,4 +22,8 @@ export const findUserById = async (userId: string) => {
 
 export const findUser = async (query: Object) => {
   return await userRepository.findOneBy(query);
+};
+
+export const updateUserById = async (userId: string, updatedFields: {}) => {
+  return await userRepository.update({ _id: userId }, updatedFields);
 };
