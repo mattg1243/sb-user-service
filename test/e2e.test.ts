c37d91b74ea756a298d22dbc6c8e0d572ced6a15
Matt Gallucci <mattgallucci97@gmail.com>
2023-04-25 21:20:24 -0700

    start of e2e suite for all routes

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -1,11 +1,31 @@
-import router from '../src/routes';
 import request from 'supertest';
-import express from 'express';
+import { app } from '../src/app';
+import { initDBConnection } from '../src/database/dataSource';
 
-const app = express();
-app.use('/', router);
+const TEST_USER = 'aafea35c-fa74-46eb-99da-561f1661dca5';
 
-test('index route works', (done) => {
-  request(app).get('/').expect('Content-Type', /json/);
-  done();
+describe('User handlers', () => {
+  beforeAll(async () => {
+    await initDBConnection();
+  });
+  test('GET /?id', (done) => {
+    request(app)
+      .get(`/?id=${TEST_USER}`)
+      .then((res) => {
+        const data = res.body;
+        expect(data).toHaveProperty('_id');
+        expect(data).toHaveProperty('artistName');
+        expect(data).toHaveProperty('email');
+        done();
+      });
+  });
+  test('GET /avatar', (done) => {
+    request(app)
+      .get(`/avatar?id=${TEST_USER}`)
+      .then((res) => {
+        const data = res.body;
+        expect(data).toContain('images/');
+        done();
+      });
+  });
 });
