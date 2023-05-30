634fa3733a930136515acc299a84c57b2589b91a
Matt Gallucci <mattgallucci97@gmail.com>
2023-05-30 16:17:27 -0700

    added route tests

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -1,12 +1,19 @@
 import request from 'supertest';
 import { app } from '../src/app';
-import { initDBConnection } from '../src/database/dataSource';
+import { closeConnection as closeDBConnection, initDBConnection } from '../src/database/dataSource';
+import { signJwt } from '../src/jwt';
 
 const TEST_USER = 'aafea35c-fa74-46eb-99da-561f1661dca5';
+const TEST_TOKEN = signJwt(TEST_USER, 'ACCESS_PRIVATE_KEY', { expiresIn: 10000 });
 
 describe('User handlers', () => {
   beforeAll(async () => {
     await initDBConnection();
+  });
+  afterAll((done) => {
+    closeDBConnection()
+      .then(() => app.disable('cancel'))
+      .then(() => done());
   });
   test('GET /?id', (done) => {
     request(app)
@@ -26,6 +33,59 @@ describe('User handlers', () => {
         const data = res.body;
         expect(data).toContain('images/');
         done();
+      });
+  });
+  test('POST /login', (done) => {
+    request(app)
+      .post('/login')
+      .send({ email: 'sbtester@gmail.com' })
+      .then((res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+  });
+  // need to write delete user route to clean up register test
+  test('GET /followers', (done) => {
+    request(app)
+      .get(`/followers?user=${TEST_USER}`)
+      .then((res) => {
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty('followers');
+        done();
+      });
+  });
+  test('GET /following', (done) => {
+    request(app)
+      .get(`/following?user=${TEST_USER}`)
+      .then((res) => {
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty('following');
+        done();
+      });
+  });
+  test('GET /isfollowing', (done) => {
+    request(app)
+      .get(`/isfollowing?user=${TEST_USER}&userToCheck=c690083b-4597-478a-9e15-68c61789807c`)
+      .then((res) => {
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty('isFollowing', true);
+      });
+    request(app)
+      .get(`/isfollowing?user=${TEST_USER}&userToCheck=abc-123`)
+      .then((res) => {
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty('isFollowing', false);
+        done();
+      });
+  });
+  test('POST /update', (done) => {
+    request(app)
+      .post('/update')
+      .set('Authorization', TEST_TOKEN)
+      .send({ artistName: 'Himothy', bio: 'Im Him' })
+      .then((res) => {
+        expect(res.status).toBe(200);
+        console.log(res.body);
       });
   });
 });
