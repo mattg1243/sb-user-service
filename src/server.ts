561e7da727f876b2f25d1ca4882dbe0d67d20420
Matt Gallucci <mattgallucci97@gmail.com>
2023-06-23 16:03:19 -0700

    migrations run on server start

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import { app } from './app';
-import { initDBConnection } from './database/dataSource';
+import { AppDataSource, initDBConnection } from './database/dataSource';
 
-let PORT_GRPC = process.env.PORT_GRPC || '4080';
+// let PORT_GRPC = process.env.PORT_GRPC || '4080';
 let PORT_HTTP = process.env.PORT_HTTP || '8080';
 
 // connect to database
-initDBConnection();
+initDBConnection().then(() => {
+  // run migrations when connection is succesfu
+  AppDataSource.runMigrations();
+});
 // start server
 app.listen(PORT_HTTP, () => {
   console.log(`HTTP User server listening on port ${PORT_HTTP}...`);
