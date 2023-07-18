1593e3523bcf2f05db8a00d166c605f9fec6141d
Matt Gallucci <mattgallucci97@gmail.com>
2023-07-18 12:56:34 -0700

    config

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,4 @@ export const BASIC_SUB_MONTHLY_CREDITS = 3;
 export const STD_SUB_MONTHLY_CREDITS = 7;
 export const PREM_SUB_MONTHLY_CREDITS = 12;
 export const UNLIMITED_LICENSE_COST = 1;
-// export const LIMITED_LICENSE_COST = 1;
+export const LIMITED_LICENSE_COST = 3;
