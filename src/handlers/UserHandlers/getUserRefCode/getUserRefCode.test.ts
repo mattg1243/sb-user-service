a4de03ca2917fca4666be85a0bfb5b3370b2da3f
Matt Gallucci <mattgallucci97@gmail.com>
2023-11-21 14:44:28 -0800

    test for handler

--- src/handlers/UserHandlers/getUserRefCode/getUserRefCode.test.ts
+++ src/handlers/UserHandlers/getUserRefCode/getUserRefCode.test.ts
@@ -1,1 +1,20 @@
-export {};
+import request from 'supertest';
+import { app } from '../../../app';
+import { closeConnection, initDBConnection } from '../../../database/dataSource';
+
+const TEST_USER = { id: 'aafea35c-fa74-46eb-99da-561f1661dca5', email: 'sbtester@gmail.com', artistName: 'SantaBihh' };
+
+describe('getUserRefCode', () => {
+  beforeAll(async () => {
+    await initDBConnection();
+  });
+  afterAll((done) => {
+    closeConnection()
+      .then(() => app.disable('cancel'))
+      .then(() => done());
+  });
+  test('GET /sub-ref-code', (done) => {
+    request(app)
+    .get('/sub-ref-code')
+  })
+});
