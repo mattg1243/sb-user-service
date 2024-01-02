ce79a02347a9a1995e39beeec339041c15ef5d04
Matt Gallucci <mattgallucci97@gmail.com>
2024-01-01 19:19:21 -0800

    added more queries for payout table, typing

--- src/services/Payout.service.ts
+++ src/services/Payout.service.ts
@@ -1,3 +1,4 @@
+import { FindOptionsWhere, FindOptionsSelect, FindOptionsSelectByString, In, Between } from 'typeorm';
 import { AppDataSource } from '../database/dataSource';
 import Payout from '../database/models/Payout.entity';
 import { ICreatePayoutArgs } from '../database/schemas/Payout.schema';
@@ -12,10 +13,31 @@ export const getPayout = async (id: string) => {
   return await payoutRepository.findOneBy({ _id: id });
 };
 
-export const getPayouts = async () => {
-  return await payoutRepository.find();
+interface IGetPayoutsArg {
+  where?: FindOptionsWhere<Payout> | FindOptionsWhere<Payout>[];
+  select?: FindOptionsSelect<Payout> | FindOptionsSelectByString<Payout>;
+}
+
+export const getPayouts = async (args?: IGetPayoutsArg) => {
+  return await payoutRepository.find({
+    relations: { user: true },
+    select: { ...args?.select, user: { _id: true, artistName: true, payoutMethod: true } },
+    where: args?.where,
+  });
+};
+
+export const getPayoutsInDateRange = async (start: Date, end: Date, query?: IGetPayoutsArg) => {
+  return await payoutRepository.find({
+    relations: { user: true },
+    select: { ...query?.select, user: { _id: true, artistName: true, payoutMethod: true } },
+    where: { ...query?.where, created_at: Between(start, end) },
+  });
 };
 
 export const getPayoutsByUser = async (userId: string) => {
-  return await payoutRepository.findBy({ userId: userId });
+  return await payoutRepository.find({
+    relations: { user: true },
+    select: { user: { _id: true, artistName: true, payoutMethod: true } },
+    where: { user: { _id: userId } },
+  });
 };
